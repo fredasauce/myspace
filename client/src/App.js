dbf5a9f6a619c916d11ed77a88d4ca902423c3dc
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-18 15:40:06 -0600

    protectedroute and fetchuser

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Switch, Route, } from "react-router-dom"
 import { Container, } from "semantic-ui-react"
+import Navbar from "./components/Navbar"
+import FetchUser from "./components/FetchUser"
+import ProtectedRoute from "./components/ProtectedRoute"
 import Home from "./components/Home"
 import NoMatch from "./components/NoMatch"
-import Navbar from "./components/Navbar"
 import Login from "./components/Login"
 import Register from "./components/Register"
 
 const App = () => (
   <> 
     <Navbar />
-    <Container>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} /> */}
-        <Route component={NoMatch} />
-      </Switch>
-    </Container>
+    <FetchUser>
+      <Container>
+        <Switch>
+          <ProtectedRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} /> */}
+          <Route component={NoMatch} />
+        </Switch>
+      </Container>
+    </FetchUser>
   </>
 )
 
